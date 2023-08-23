ecb2d38ae3e0a7e65613ceebc993412e88174367
nohssam73 <nohssam73@gmail.com>
2023-08-23 13:15:14 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// 1.App 를 BrowserRouter 로 감싼다.
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
        <Header />
-       <DayList />
-       <Day />
+        <Routes>
+          {/* "/"는 첫 페이지를 의미한다. */}
+          <Route path="/" element={<DayList />} />
+          <Route path="/day" element={<Day />} />
+       </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/component/Day.js
+++ src/component/Day.js
@@ -2,7 +2,7 @@ import dummy from "../db/data.json";
 
 export default function Day() {
     // 날짜가 1인 것만 출력하기 위함
-    const day = 3 ;
+    const day = 1 ;
     // filter는 해석 그대로 걸러주는 역할을 하는 함수입니다. 
     // 주로 특정 조건을 만족하는 새로운 배열을 필요로 할 때 사용하는 편입니다. 
     // SQL로 치면 where절에서 하는 행위를 하는 함수입니다. 
