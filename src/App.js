4a9ffdd52e398cdac3ee23335afe69c1dd9ed475
nohssam73 <nohssam73@gmail.com>
2023-08-25 13:20:44 +0900

    useReducer 완성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Day from "./component/Day";
 import Main02 from "./component/Main02";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptyPage";
+import Main03 from "./component/Main03";
 
 // 1.App 를 BrowserRouter 로 감싼다.
 function App() {
@@ -14,11 +15,13 @@ function App() {
       <hr />
       <button style={{marginLeft:"20px"}}><Link to="/main01"> Main01 </Link> </button>
       <button style={{marginLeft:"20px"}}><Link to="/main02"> Main02 </Link> </button>
+      <button style={{marginLeft:"20px"}}><Link to="/main03"> Main03 </Link> </button>
     </div>
       <Routes>
         <Route path="/" />
         <Route path="/main01" element={<Main01 />} />
         <Route path="/main02" element={<Main02 />} />
+        <Route path="/main03" element={<Main03 />} />
         <Route path="/day/:day" element={<Day />} />
         <Route path="/dayList" element={<DayList />} />
         {/* 이외에 url이 들어오면 받아들이는 페이지 */}
