7e1b767081f63fcec317e5bf5327686df4b21e09
nohssam73 <nohssam73@gmail.com>
2023-08-28 10:26:07 +0900

    Main04_완성

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main02 from "./component/Main02";
 import DayList from "./component/DayList";
 import EmptyPage from "./component/EmptyPage";
 import Main03 from "./component/Main03";
+import Main04 from "./component/Main04";
 
 // 1.App 를 BrowserRouter 로 감싼다.
 function App() {
@@ -13,15 +14,17 @@ function App() {
     <div className="App">
       <h1>  REACT 연습하기</h1>
       <hr />
-      <button style={{marginLeft:"20px"}}><Link to="/main01"> Main01 </Link> </button>
-      <button style={{marginLeft:"20px"}}><Link to="/main02"> Main02 </Link> </button>
-      <button style={{marginLeft:"20px"}}><Link to="/main03"> Main03 </Link> </button>
+      <button style={{marginLeft:"10px"}}><Link to="/main01"> Main01 </Link> </button>
+      <button style={{marginLeft:"10px"}}><Link to="/main02"> Main02 </Link> </button>
+      <button style={{marginLeft:"10px"}}><Link to="/main03"> Main03 </Link> </button>
+      <button style={{marginLeft:"10px"}}><Link to="/main04"> Main04 </Link> </button>
     </div>
       <Routes>
         <Route path="/" />
         <Route path="/main01" element={<Main01 />} />
         <Route path="/main02" element={<Main02 />} />
         <Route path="/main03" element={<Main03 />} />
+        <Route path="/main04" element={<Main04 />} />
         <Route path="/day/:day" element={<Day />} />
         <Route path="/dayList" element={<DayList />} />
         {/* 이외에 url이 들어오면 받아들이는 페이지 */}
